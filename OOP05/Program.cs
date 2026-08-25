3470d641452c28baf2ff71905b379b3a70eda028
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 01:07:57 +0300

    Theoretical

--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -9,46 +9,65 @@ namespace OOP05
         {
 
 
-            #region
-
-
-            Q1
-(A) both variables refer to the same object in memory
-
-(B) No, it does not create a new object.It only copies the reference to the same object
-
-(C) Copying a reference means two variables refer to the same object Copying an object creates a separate object with its own data
+            #region Theoretical
 
 
 
-                Q2
-(A) A shallow copy creates a new object and copies all value-type fields
-For reference - type fields,only the references(links)are copied-both objects point to the same nested object
+            //   Q1
+            //(A) both variables refer to the same object in memory
 
-(B)A deep copy creates a new object and recursively copies all nested objects
-The original and the copy are completely independent
+            //(B) No, it does not create a new object.It only copies the reference to the same object
 
-                (C) They are shared between the original object and the copied object
-
-(D) New independent copies of the reference-type members are created
-
-(E) Deep Copy is safer when we want to modify the copied object’s data without affecting the original object
+            //(C) Copying a reference means two variables refer to the same object Copying an object creates a separate object with its own data
 
 
 
-                Q3
+            //                Q2
+            //(A) A shallow copy creates a new object and copies all value - type fields
+            //For reference - type fields,only the references(links)are copied-both objects point to the same nested object
 
-(A) A static field belongs to the class and is shared by all objects.An instance field belongs to each individual object
+            //(B) A deep copy creates a new object and recursively copies all nested objects
+            //The original and the copy are completely independent
 
-(B) static method belongs to the class and can be called without creating an object.No, it cannot directly access instance members
+            //                (C) They are shared between the original object and the copied object
 
-(C) static constructor is used to initialize static members.It is executed automatically once before the class is used for the first time
+            //(D) New independent copies of the reference - type members are created
 
-(D) A static class contains only static members No, we cannot create an object from a static class
-
-
+            //(E) Deep Copy is safer when we want to modify the copied object’s data without affecting the original object
 
 
+
+            //                Q3
+
+            //(A) A static field belongs to the class and is shared by all objects.An instance field belongs to each individual object
+
+            //(B) static method belongs to the class and can be called without creating an object.No, it cannot directly access instance members
+
+            //(C) static constructor is used to initialize static members.It is executed automatically once before the class is used for the first time
+
+            //(D) A static class contains only static members No, we cannot create an object from a static class
+
+
+
+            //            Q4
+            //(A) An Extension Method allows us to add a new method to an existing class without modifying the original class
+
+            //(B) The this keyword
+
+            //(C) it must be declared inside a static class
+
+            //(D) No, an extension method cannot directly access private members
+
+
+
+            //            Q5
+            //(A) A Partial Class is a class that can be split into multiple files using the partial keyword
+
+            //(B) To organize large classes and separate different parts of their functionality
+
+            //(C) A Partial Method is a method declared in one part of a partial class and can be implemented in another part
+
+            // (D) It is ignored by the compiler and does not cause an error
 
 
 
