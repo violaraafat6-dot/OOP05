7e4fccfe0c64187a64831728f8a3f35690745363
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 04:28:09 +0300

    DeliveryCenter

--- OOP05/DeliveryCenter.cs
+++ OOP05/DeliveryCenter.cs
@@ -4,120 +4,120 @@ using System.Text;
 
 #region DeliveryCenter
 
-namespace OOP005
-{
-    public class DeliveryCenter
-    {
-        public string CenterName { get; set; }
+//namespace OOP005
+//{
+//    public class DeliveryCenter
+//    {
+//        public string CenterName { get; set; }
 
-        public Driver Driver { get; set; }
+//        public Driver Driver { get; set; }
 
-        private Shipment[] shipments =
-            new Shipment[20];
+//        private Shipment[] shipments =
+//            new Shipment[20];
 
-        public Shipment this[int index]
-        {
-            get
-            {
-                if (index >= 0 &&
-                    index < shipments.Length)
-                    return shipments[index];
+//        public Shipment this[int index]
+//        {
+//            get
+//            {
+//                if (index >= 0 &&
+//                    index < shipments.Length)
+//                    return shipments[index];
 
-                return null;
-            }
+//                return null;
+//            }
 
-            set
-            {
-                if (index >= 0 &&
-                    index < shipments.Length)
-                    shipments[index] = value;
-            }
-        }
+//            set
+//            {
+//                if (index >= 0 &&
+//                    index < shipments.Length)
+//                    shipments[index] = value;
+//            }
+//        }
 
-        public Shipment this[string trackingCode]
-        {
-            get
-            {
-                for (int i = 0;
-                     i < shipments.Length;
-                     i++)
-                {
-                    if (shipments[i] != null &&
-                        shipments[i].TrackingCode ==
-                        trackingCode)
-                    {
-                        return shipments[i];
-                    }
-                }
+//        public Shipment this[string trackingCode]
+//        {
+//            get
+//            {
+//                for (int i = 0;
+//                     i < shipments.Length;
+//                     i++)
+//                {
+//                    if (shipments[i] != null &&
+//                        shipments[i].TrackingCode ==
+//                        trackingCode)
+//                    {
+//                        return shipments[i];
+//                    }
+//                }
 
-                return null;
-            }
-        }
+//                return null;
+//            }
+//        }
 
-        public bool AddShipment(Shipment shipment)
-        {
-            for (int i = 0;
-                 i < shipments.Length;
-                 i++)
-            {
-                if (shipments[i] == null)
-                {
-                    shipments[i] = shipment;
-                    return true;
-                }
-            }
+//        public bool AddShipment(Shipment shipment)
+//        {
+//            for (int i = 0;
+//                 i < shipments.Length;
+//                 i++)
+//            {
+//                if (shipments[i] == null)
+//                {
+//                    shipments[i] = shipment;
+//                    return true;
+//                }
+//            }
 
-            return false;
-        }
+//            return false;
+//        }
 
-        public bool RemoveShipment(
-            string trackingCode)
-        {
-            for (int i = 0;
-                 i < shipments.Length;
-                 i++)
-            {
-                if (shipments[i] != null &&
-                    shipments[i].TrackingCode ==
-                    trackingCode)
-                {
-                    shipments[i] = null;
-                    return true;
-                }
-            }
+//        public bool RemoveShipment(
+//            string trackingCode)
+//        {
+//            for (int i = 0;
+//                 i < shipments.Length;
+//                 i++)
+//            {
+//                if (shipments[i] != null &&
+//                    shipments[i].TrackingCode ==
+//                    trackingCode)
+//                {
+//                    shipments[i] = null;
+//                    return true;
+//                }
+//            }
 
-            return false;
-        }
+//            return false;
+//        }
 
-        public void PrintAllShipments()
-        {
-            for (int i = 0;
-                 i < shipments.Length;
-                 i++)
-            {
-                if (shipments[i] != null)
-                {
-                    shipments[i].PrintShipment();
-                    Console.WriteLine();
-                }
-            }
-        }
+//        public void PrintAllShipments()
+//        {
+//            for (int i = 0;
+//                 i < shipments.Length;
+//                 i++)
+//            {
+//                if (shipments[i] != null)
+//                {
+//                    shipments[i].PrintShipment();
+//                    Console.WriteLine();
+//                }
+//            }
+//        }
 
-        public void PrintTrackingStatuses()
-        {
-            for (int i = 0;
-                 i < shipments.Length;
-                 i++)
-            {
-                if (shipments[i] is ITrackable trackable)
-                {
-                    Console.WriteLine(
-                        trackable.GetTrackingStatus());
-                }
-            }
-        }
-    }
-}
+//        public void PrintTrackingStatuses()
+//        {
+//            for (int i = 0;
+//                 i < shipments.Length;
+//                 i++)
+//            {
+//                if (shipments[i] is ITrackable trackable)
+//                {
+//                    Console.WriteLine(
+//                        trackable.GetTrackingStatus());
+//                }
+//            }
+//        }
+//    }
+//}
 
 
 #endregion
