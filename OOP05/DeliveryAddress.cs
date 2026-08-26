2f37c813b13e30fdf53c28803b2bd15a4d0b4266
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 04:29:21 +0300

    Deliveryaadeess

--- OOP05/DeliveryAddress.cs
+++ OOP05/DeliveryAddress.cs
@@ -6,37 +6,37 @@ using System.Text;
 #region Deliveryaadeess
 
 
-namespace OOP005
-{
-    public class DeliveryAddress
-    {
-        public string City { get; set; }
-        public string Street { get; set; }
-        public int BuildingNumber { get; set; }
+//namespace OOP005
+//{
+//    public class DeliveryAddress
+//    {
+//        public string City { get; set; }
+//        public string Street { get; set; }
+//        public int BuildingNumber { get; set; }
 
-        public DeliveryAddress(
-            string city,
-            string street,
-            int buildingNumber)
-        {
-            City = city;
-            Street = street;
-            BuildingNumber = buildingNumber;
-        }
+//        public DeliveryAddress(
+//            string city,
+//            string street,
+//            int buildingNumber)
+//        {
+//            City = city;
+//            Street = street;
+//            BuildingNumber = buildingNumber;
+//        }
 
-        public string GetFullAddress()
-        {
-            return $"{BuildingNumber} {Street}, {City}";
-        }
+//        public string GetFullAddress()
+//        {
+//            return $"{BuildingNumber} {Street}, {City}";
+//        }
 
-        public DeliveryAddress Copy()
-        {
-            return new DeliveryAddress(
-                City,
-                Street,
-                BuildingNumber);
-        }
-    }
-}
+//        public DeliveryAddress Copy()
+//        {
+//            return new DeliveryAddress(
+//                City,
+//                Street,
+//                BuildingNumber);
+//        }
+//    }
+//}
 
 #endregion
