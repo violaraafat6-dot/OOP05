5ef6e656ddb3d6535a21a4bed0dbde5eb863b077
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 04:30:09 +0300

    CompletedShipment

--- OOP05/CompletedShipment.cs
+++ OOP05/CompletedShipment.cs
@@ -4,44 +4,44 @@ using System.Text;
 
 # region CompletedShipment
 
-namespace OOP005
-{
-    public sealed class CompletedShipment : Shipment
-    {
-        public CompletedShipment(
-            string trackingCode,
-            string description,
-            decimal weight,
-            decimal deliveryFee,
-            DeliveryAddress destination)
-            : base(
-                trackingCode,
-                description,
-                weight,
-                deliveryFee,
-                destination)
-        {
-            SetInitialTrackingStatus("Delivered");
-        }
+//namespace OOP005
+//{
+//    public sealed class CompletedShipment : Shipment
+//    {
+//        public CompletedShipment(
+//            string trackingCode,
+//            string description,
+//            decimal weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination)
+//            : base(
+//                trackingCode,
+//                description,
+//                weight,
+//                deliveryFee,
+//                destination)
+//        {
+//            SetInitialTrackingStatus("Delivered");
+//        }
 
-        public override decimal EstimatedCost
-        {
-            get
-            {
-                return DeliveryFee + (Weight * 5);
-            }
-        }
+//        public override decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + (Weight * 5);
+//            }
+//        }
 
-        public override void PrintShipment()
-        {
-            Console.WriteLine("Completed Shipment");
-            Console.WriteLine($"Tracking Code : {TrackingCode}");
-            Console.WriteLine($"Description : {Description}");
-            Console.WriteLine($"Weight : {Weight} KG");
-            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
-            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
-        }
-    }
-}
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("Completed Shipment");
+//            Console.WriteLine($"Tracking Code : {TrackingCode}");
+//            Console.WriteLine($"Description : {Description}");
+//            Console.WriteLine($"Weight : {Weight} KG");
+//            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+//            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
+//        }
+//    }
+//}
 
 #endregion
