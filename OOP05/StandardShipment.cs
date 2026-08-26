18fd5049566cc915e993a16b3eac9414adc1d791
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 04:16:36 +0300

    StandardShipment

--- OOP05/StandardShipment.cs
+++ OOP05/StandardShipment.cs
@@ -4,56 +4,56 @@ using System.Text;
 
 #region StandardShipment
 
-namespace OOP005
-{
-    public class StandardShipment :
-        Shipment, ITrackable, IInsurable
-    {
-        public StandardShipment(
-            string trackingCode,
-            string description,
-            decimal weight,
-            decimal deliveryFee,
-            DeliveryAddress destination)
-            : base(
-                trackingCode,
-                description,
-                weight,
-                deliveryFee,
-                destination)
-        {
-            SetInitialTrackingStatus("In Transit");
-        }
+//namespace OOP005
+//{
+//    public class StandardShipment :
+//        Shipment, ITrackable, IInsurable
+//    {
+//        public StandardShipment(
+//            string trackingCode,
+//            string description,
+//            decimal weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination)
+//            : base(
+//                trackingCode,
+//                description,
+//                weight,
+//                deliveryFee,
+//                destination)
+//        {
+//            SetInitialTrackingStatus("In Transit");
+//        }
 
-        public override decimal EstimatedCost
-        {
-            get
-            {
-                return DeliveryFee + (Weight * 5);
-            }
-        }
+//        public override decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + (Weight * 5);
+//            }
+//        }
 
-        public override void PrintShipment()
-        {
-            Console.WriteLine("Standard Shipment");
-            Console.WriteLine($"Tracking Code : {TrackingCode}");
-            Console.WriteLine($"Description : {Description}");
-            Console.WriteLine($"Weight : {Weight} KG");
-            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
-            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
-        }
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("Standard Shipment");
+//            Console.WriteLine($"Tracking Code : {TrackingCode}");
+//            Console.WriteLine($"Description : {Description}");
+//            Console.WriteLine($"Weight : {Weight} KG");
+//            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+//            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
+//        }
 
-        string ITrackable.GetTrackingStatus()
-        {
-            return $"Shipment {TrackingCode} is {TrackingStatus}.";
-        }
+//        string ITrackable.GetTrackingStatus()
+//        {
+//            return $"Shipment {TrackingCode} is {TrackingStatus}.";
+//        }
 
-        public decimal CalculateInsurance()
-        {
-            return EstimatedCost * 0.05m;
-        }
-    }
-}
+//        public decimal CalculateInsurance()
+//        {
+//            return EstimatedCost * 0.05m;
+//        }
+//    }
+//}
 
 
 #endregion
