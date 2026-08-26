25eacbc082871404934561f71426068ced37d66b
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 04:25:32 +0300

    DeliveryUtilities

--- OOP05/DeliveryUtilities.cs
+++ OOP05/DeliveryUtilities.cs
@@ -4,25 +4,25 @@ using System.Text;
 
 #region DeliveryUtilities
 
-namespace OOP005
-{
-    public static class DeliveryUtilities
-    {
-        public static void PrintSeparator()
-        {
-            Console.WriteLine(
-                "==========================================");
-        }
+//namespace OOP005
+//{
+//    public static class DeliveryUtilities
+//    {
+//        public static void PrintSeparator()
+//        {
+//            Console.WriteLine(
+//                "==========================================");
+//        }
 
-        public static void PrintSystemTitle()
-        {
-            PrintSeparator();
-            Console.WriteLine(
-                "Smart Delivery Management System");
-            PrintSeparator();
-        }
-    }
-}
+//        public static void PrintSystemTitle()
+//        {
+//            PrintSeparator();
+//            Console.WriteLine(
+//                "Smart Delivery Management System");
+//            PrintSeparator();
+//        }
+//    }
+//}
 
 
 #endregion
