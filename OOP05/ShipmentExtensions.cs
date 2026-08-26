01c0ab50fa7ec41189604f698371942a499ef486
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-26 04:18:47 +0300

    ShipmentExtensions

--- OOP05/ShipmentExtensions.cs
+++ OOP05/ShipmentExtensions.cs
@@ -4,37 +4,37 @@ using System.Text;
 
 #region ShipmentExtensions
 
-namespace OOP005
-{
-    public static class ShipmentExtensions
-    {
-        public static string GetSummary(
-            this Shipment shipment)
-        {
-            string shipmentType;
+//namespace OOP005
+//{
+//    public static class ShipmentExtensions
+//    {
+//        public static string GetSummary(
+//            this Shipment shipment)
+//        {
+//            string shipmentType;
 
-            if (shipment is StandardShipment)
-                shipmentType = "Standard";
-            else if (shipment is ExpressShipment)
-                shipmentType = "Express";
-            else if (shipment is InternationalShipment)
-                shipmentType = "International";
-            else
-                shipmentType = "Other";
+//            if (shipment is StandardShipment)
+//                shipmentType = "Standard";
+//            else if (shipment is ExpressShipment)
+//                shipmentType = "Express";
+//            else if (shipment is InternationalShipment)
+//                shipmentType = "International";
+//            else
+//                shipmentType = "Other";
 
-            return
-                $"{shipment.TrackingCode} | " +
-                $"{shipmentType} | " +
-                $"{shipment.Weight} KG | " +
-                $"{shipment.TrackingStatus}";
-        }
+//            return
+//                $"{shipment.TrackingCode} | " +
+//                $"{shipmentType} | " +
+//                $"{shipment.Weight} KG | " +
+//                $"{shipment.TrackingStatus}";
+//        }
 
-        public static bool IsDelivered(
-            this Shipment shipment)
-        {
-            return shipment.TrackingStatus == "Delivered";
-        }
-    }
-}
+//        public static bool IsDelivered(
+//            this Shipment shipment)
+//        {
+//            return shipment.TrackingStatus == "Delivered";
+//        }
+//    }
+//}
 
 #endregion
